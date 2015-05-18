af650b8504d052804cecb0e02a3d427b7fa282ae
nina <ninajl@gmail.com>
2015-05-18 09:56:01 -0600

    Create industry action and button on form

--- app/controllers/industries_controller.rb
+++ app/controllers/industries_controller.rb
@@ -4,9 +4,20 @@ class IndustriesController < ApplicationController
   end
 
   def new
-  @industry = Industry.new
+    @industry = Industry.new
   end
 
+  def create
+    @industry = Industry.new(industry_params)
+    if @industry.save
+      redirect_to industry_path(@industry), notice: "Industry was created successfully"
+    else
+      render :new
+    end
+  end
 
-
+  private
+  def industry_params
+    params.require(:industry).permit(:name, :description)
+  end
 end
