c4b0076718fbd3b6ed966e77fb6674b723de7ae0
nina <ninajl@gmail.com>
2015-05-18 15:43:14 -0600

    Add update action

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -28,7 +28,15 @@ class CompaniesController < ApplicationController
     @company = Company.find(params[:id])
   end
 
-  
+  def update
+    @company = Company.find(params[:id])
+    if @company.update(company_params)
+      redirect_to companies_path, notice: "Company was updated successfully"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def company_params
