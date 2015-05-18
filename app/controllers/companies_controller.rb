b6f498673bfc5cfada2d0a8985d6e69970771e19
nina <ninajl@gmail.com>
2015-05-18 11:47:11 -0600

    add company create action

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -8,4 +8,19 @@ class CompaniesController < ApplicationController
     @company = Company.new
   end
 
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to company_path(@company), notice: "Company was created successfully"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :description, :price, :industry_id)
+  end
+
 end
